450325b5f2910682fe628dac8fbd3bf95953a8d5
Mattia Sabato <129496313+Mattizza@users.noreply.github.com>
2023-05-24 13:24:08 +0200

    Improved efficieny, changed candidates evaluation

--- PrunedCV.py
+++ PrunedCV.py
@@ -9,14 +9,14 @@ class PrunedCV:
 
     def __init__(self, X_train: np.ndarray, y_train: np.ndarray, folds: KFold | StratifiedKFold):
         '''
-        Initialize a new instance of the class. It creates a PrunedCV object that can be used 
-        to perform either model selection and model validation.
+        Initialize a new instance of the class. It creates a PrunedCV object that can be used to perform either model selection
+        and model validation.
 
         Parameters
         ---
         X_train : np.array
-            Data to cross-validate. It can be either the whole dataset (training/test) or just 
-            the training set (training/validation/set).
+            Data to cross-validate. It can be either the whole dataset (training/test) or just the training set
+            (training/validation/set).
 
         y_train : np.array
             Labels to cross-validate, same distinction as `X_train`.
@@ -31,8 +31,8 @@ class PrunedCV:
 
     def set_params(self, param_grid: dict, scores: list) -> None:
         '''
-        Pass the hyperparameters to tune and the scores used to choose the best ones. Notice that also a `sklearn`
-        model is needed. All the information must be stored as nested dictionaries. Examples in the following.
+        Pass the hyperparameters to tune and the scores used to choose the best ones. Notice that also a `sklearn` model is
+        needed. All the information must be stored as nested dictionaries. Examples in the following.
 
         Parameters
         ---
@@ -64,8 +64,8 @@ class PrunedCV:
     def set_evaluation(self, score: metrics = metrics.accuracy_score, min_folds: int = 0, 
                        thresh_skip: int = 0, thresh_percentage: float = 0.0) -> None:
         '''
-        Set the evaluation scores used during the procedure. It is also possible to set thresholds regarding
-        the pruning steps. If no thresholds are passed, the pruning step will not be implemented.
+        Set the evaluation scores used during the procedure. It is also possible to set thresholds regarding the pruning steps.
+        If no thresholds are passed, the pruning step will not be implemented.
 
         Parameters
         ---
@@ -73,17 +73,16 @@ class PrunedCV:
             List of scores used to evaluate the goodness of a model. They have to be `sklearn.metrcis` methods.
         
         min_folds : int, default = 0
-            Minimum number of folds needed to compute the average between the performances and make a fair
-            comparison with the best performance.
+            Minimum number of folds needed to compute the average between the performances and make a fair comparison with the
+            best performance.
         
         thresh_skip : int, default = 0
-            How many times the actual model should be below the percentage of the maximal value in order to early
-            stop the cross-validation and go to the next configuration.
+            How many times the actual model should be below the percentage of the maximal value in order to early stop the
+            cross-validation and go to the next configuration.
         
         thresh_percentage : float, default = 0.0
-            The percentage of the maximal value which is needed to be reached to continue cross-validating
-            with that specific model. It can greatly lower the computational cost, but if too small it can also
-            skip potentially good models.            
+            The percentage of the maximal value which is needed to be reached to continue cross-validating with that specific
+            model. It can greatly lower the computational cost, but if too small it can also skip potentially good models.            
         '''
 
         self.__thresh_skip__       = thresh_skip
@@ -91,16 +90,15 @@ class PrunedCV:
         self.__score__             = score.__name__
         self.__min_folds__         = min_folds
     
-    def __evaluate_model__(self, X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray, y_test: np.ndarray, 
+    def __evaluate_model__(self, X_train: np.ndarray, y_train: np.ndarray, X_test: np.ndarray, y_test: np.ndarray,
                            model: type, scores: list) -> dict:
         
 
         model.fit(X_train, y_train)                  
         y_hat = model.predict(X_test)    
 
-        # Exploit list comprehension to compute each score.            
-        results = [score(y_test, y_hat) for score in scores]
-        # Store the results into a dictionary where each key is a score.
+        # Exploit list comprehension to compute each score and store the results into a dictionary where each key is a score.
+        results     = [score(y_test, y_hat) for score in scores]
         dic_results =  {k: v for k, v in zip([score.__name__ for score in scores], results)}
         
         return dic_results
@@ -120,9 +118,9 @@ class PrunedCV:
             * `4` = print any detail about the folds.
         '''
 
+        # Initialize best score and models performances.
+        best               = 0
         models_performance = {}
-        # Initialize best score.
-        skip_max = 0
 
         # Iterate over all models of interest.
         for model_str in self.__param_grid__.keys():                      
@@ -135,17 +133,13 @@ class PrunedCV:
             model  = getattr(module, model_name)                                     
                                                                                     
             models_performance[model_name] = {}
-            if verbose >= 1:
-                print(f"Model: {model_str}\n")
+            print(f"Model: {model_str}\n") if verbose >= 1 else None
             
             # For each model, iterate over all possible configuration.
             for config in ParameterGrid(self.__param_grid__[model_str]):                                                                         
                                                         
-                if verbose >= 2:
-                    print("\n\tNEW CONFIGURATION")
-                    
-                    if verbose >= 3:
-                        print(f"\nConfiguration: {config}\n")
+                print("\n\tNEW CONFIGURATION")        if verbose >= 2 else None
+                print(f"\nConfiguration: {config}\n") if verbose >= 3 else None
 
                 model_config_name = model_name + f"_{count_config}"
 
@@ -154,18 +148,17 @@ class PrunedCV:
                                                                      for score in [score.__name__ for score in self.__scores__]}
                 models_performance[model_name][model_config_name]['weight'] = []                                         
                                 
-                skip            = False
-                best            = False
-                count_skip      = 0                                                                                     
-                count_fold      = 1
-                count_candidate = 0
+                skipped    = False
+                count_skip = 0                                                                                     
+                count_fold = 1
 
                 # Start the evaluation of the model using the folds.
                 for train_indices, valid_indices in self.__folds__.split(self.__X_train__, self.__y_train__):
                     
                     # If the model has already reached bad performances #thresh_skip times, early terminate the process.
-                    if count_skip >= self.__thresh_skip__ and self.__thresh_skip__ != 0.0:
+                    if count_skip == self.__thresh_skip__ and self.__thresh_skip__ != 0.0:
 
+                        skipped = True
                         break
                                                                     
                     X_train_fold = self.__X_train__[train_indices]      
@@ -176,7 +169,7 @@ class PrunedCV:
                     # Train the classifier and evaluate it.                                                      
                     clf = model(**config)                                           
                     results = self.__evaluate_model__(X_train_fold, y_train_fold, X_valid_fold, y_valid_fold,
-                                                  clf, self.__scores__)
+                                                      clf, self.__scores__)
                     
                     # Append the scores to the dictionary.
                     for score, result in results.items():
@@ -187,45 +180,32 @@ class PrunedCV:
                     models_performance[model_name][model_config_name]['weight'].append(len(train_indices))
                     
                     # Every new fold, compute the average of the scores.
-                    dummy_1 = models_performance[model_name][model_config_name][self.__score__]
-                    dummy_2 = models_performance[model_name][model_config_name]['weight']
-                    avg_performance = np.average(dummy_1, weights = dummy_2)
+                    actual_scores          = models_performance[model_name][model_config_name][self.__score__]
+                    actual_weights         = models_performance[model_name][model_config_name]['weight']
+                    actual_avg_performance = np.average(actual_scores, weights = actual_weights)
 
                     # If the model has a bad performance, increase the count by 1.
-                    if avg_performance < self.__thresh_percentage__ * skip_max:
-
-                        count_skip += 1
-                    
-                    # If the model has a good performance, increase the count by 1.
-                    elif avg_performance >= skip_max and self.__thresh_percentage__ != 0.0:
-
-                        count_candidate += 1
-
-                    # If the model has a really good performance at least #min_folds times, it becomes the new best.
-                    if avg_performance >= skip_max and count_fold >= self.__min_folds__ \
-                       and self.__thresh_percentage__ != 0.0:
-                        
-                        best = True
-                    
+                    count_skip += 1 if actual_avg_performance < self.__thresh_percentage__ * best else 0
+                                        
                     if verbose >= 4:
                         
                         print(f"Fold {count_fold} / {self.__folds__.get_n_splits()} - Skip: {count_skip} / {self.__thresh_skip__}")
                         print(f"Results: {results}")
-                        print(f"Highest average {self.__score__}: {np.round(skip_max, 4)}")
+                        print(f"Highest average {self.__score__}: {np.round(best, 4)}")
 
                     count_fold += 1
                 
-                # If a new best has been found, compute the average of its scores and update the skip_max.
-                if best:
-                    
-                    dummy_1 = models_performance[model_name][model_config_name][self.__score__]
-                    dummy_2 = models_performance[model_name][model_config_name]['weight']
-                    skip_max = np.average(dummy_1, weights = dummy_2) 
+                total_scores          = models_performance[model_name][model_config_name][self.__score__]
+                total_weights         = models_performance[model_name][model_config_name]['weight']
+                total_avg_performance = np.average(total_scores, weights = total_weights)
+
+                # If the model has really good performances, it becomes the new best.
+                best = total_avg_performance if total_avg_performance >= best and self.__thresh_percentage__ != 0.0 else best
 
                 # Store the parameters in order to be able later to retrieve the best configuration.
                 models_performance[model_name][model_config_name]['parameters'] = config
+                models_performance[model_name][model_config_name]['skipped'] = skipped
                 count_config += 1
-                models_performance[model_name][model_config_name]['skip'] = skip
 
             print('\n')
 
@@ -236,3 +216,4 @@ class PrunedCV:
         Just returns the performances.
         '''
         return self.models_perfomance
+
